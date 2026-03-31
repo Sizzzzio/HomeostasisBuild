8fbadcacb98b2ef29e470c6d3c9357d7782801ba
joshuakwon06 <159495594+joshuakwon06@users.noreply.github.com>
2026-03-31 02:09:37 -0400

    List of lines
    Reorganized fields in DialogueData class for clarity.

--- Assets/Scripts/DialogueData.cs
+++ Assets/Scripts/DialogueData.cs
@@ -1,19 +1,14 @@
 using UnityEngine;
- 
+
 [CreateAssetMenu(fileName = "NewDialogue", menuName = "Dialogue/Dialogue Data")]
 public class DialogueData : ScriptableObject
 {
-    [Header("NPC Info")]
-    public string npcName;           
-    public Sprite npcPortrait;       // Face image shown in dialogue box
- 
-    [Header("Conversation")]
-    public DialogueLine[] lines;     // All the lines in this conversation, in order
-}
+    // The name shown above the dialogue box (e.g. "Cloaked Automaton")
+    public string npcName;
 
-// All the lines this NPC says, one after another.
-    // Press Space to go to the next line.
-    [TextArea(2, 4)]
-    public string[] lines;
-}
- 
+    // Optional face image shown next to the text
+    public Sprite npcPortrait;
+
+    // All the lines this NPC says, one after another
+    // Press Space to go to the next line
+    public DialogueLine[] lines;
