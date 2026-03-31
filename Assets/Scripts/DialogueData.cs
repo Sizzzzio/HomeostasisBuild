87f30b8da58be1cf2a96414dc88bcc055121d298
joshuakwon06 <159495594+joshuakwon06@users.noreply.github.com>
2026-03-31 02:03:04 -0400

    Holds the lines of text
    Updated the DialogueData class to include npcPortrait and modified lines to be an array of strings.

--- Assets/Scripts/DialogueData.cs
+++ Assets/Scripts/DialogueData.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
  
 [CreateAssetMenu(fileName = "NewDialogue", menuName = "Dialogue/Dialogue Data")]
-public class DialogueData: ScriptableObject
+public class DialogueData : ScriptableObject
 {
     [Header("NPC Info")]
-    public string npcName;          
+    public string npcName;           
+    public Sprite npcPortrait;       // Face image shown in dialogue box
  
     [Header("Conversation")]
     public DialogueLine[] lines;     // All the lines in this conversation, in order
+}
+
+// All the lines this NPC says, one after another.
+    // Press Space to go to the next line.
+    [TextArea(2, 4)]
+    public string[] lines;
 }
  
